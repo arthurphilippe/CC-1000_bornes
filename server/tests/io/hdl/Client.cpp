d1a0444f87f5e866f28cbaad797418c15b4fd014
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-07-01 12:03:21 +0200

    testing on  client cycle and geters

--- server/tests/io/hdl/Client.cpp
+++ server/tests/io/hdl/Client.cpp
@@ -15,6 +15,7 @@ class DumbClient : public io::hdl::Client {
 public:
 	DumbClient(io::Selector &stor, int fd) : Client(stor, fd) {}
 	std::list<std::string> &getMsgs() { return _receivedMsgs; }
+	void setFd(int fd) { _fd = fd; }
 };
 
 Test(ioHdlClient, 1_rcvdMsgs)
@@ -46,4 +47,26 @@ Test(ioHdlClient, 1_rcvdMsgs)
 	cr_assert_eq(msgs.empty(), true);
 
 	close(filedes[1]);
+}
+
+Test(ioHdlClient, 2_getters)
+{
+	io::Selector stor;
+	int filedes[2];
+
+	if (pipe(filedes) == -1) cr_assert_fail("failed to pipe: errno %d");
+	DumbClient client(stor, filedes[0]);
+
+	cr_assert_eq(client.getFd(), filedes[0]);
+	cr_assert_eq(client.live(), true);
+
+	dprintf(filedes[1], "%s",
+		"kappa\n\rwa\nkawaaaaaaaaaaaaaaaaaaaaaaiiiiiiiiiiiiiiiii\n"
+		"\n");
+
+	client.onRead();
+
+	client.setFd(filedes[1]);
+	client.onCycle();
+	close(filedes[0]);
 }
