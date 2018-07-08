3a4bb8f4f41be1e876acce6847b50943facf32eb
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-07-08 14:25:18 +0200

    reworked main

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -10,12 +10,26 @@
 #include "io/Selector.hpp"
 #include "io/hdl/Listener.hpp"
 
-int main()
+int main(int ac, char **av)
 {
-	game::Manager stor;
-	std::unique_ptr<io::hdl::IHandle> listener(
-		new io::hdl::Listener(stor, 4242));
-	stor.registerHandle(listener);
-	stor.loop();
-	std::cout << "Hello Jafar" << std::endl;
+	auto ret(0);
+	if (ac >= 2) {
+		try {
+
+			auto portnb(std::stoi(av[1]));
+			game::Manager stor;
+			std::unique_ptr<io::hdl::IHandle> listener(
+				new io::hdl::Listener(stor, portnb));
+			stor.registerHandle(listener);
+			stor.loop();
+		} catch (std::exception &e) {
+			e.what();
+			ret = 1;
+		}
+	} else {
+		std::cerr << av[0] << ": please provide a port number"
+			  << std::endl;
+		ret = 1;
+	}
+	return ret;
 }
