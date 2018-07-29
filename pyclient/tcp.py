ba45afd849ad936d044cd50b93ee15adaf1d0c85
Arthur Philippe <arthur.philippe@epitech.eu>
2018-07-29 12:42:25 +0200

    norm

--- pyclient/tcp.py
+++ pyclient/tcp.py
@@ -1,46 +1,59 @@
 #!/bin/python3
 
-import socket #for sockets
-import sys #for exit
+import socket  # for sockets
+import sys  # for exit
+
 
 def create():
-	try:
-		# create an AF_INET, STREAM socket (TCP)
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	except socket.error as error:
-		raise RuntimeError(error)
-	return s
+    try:
+        # create an AF_INET, STREAM socket (TCP)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as error:
+        raise RuntimeError(error)
+    return s
+
 
 def connect(sock, hostname="0.0.0.0", port=4242):
-	# Resolve the hostname
-	try:
-		remote_ip = socket.gethostbyname(hostname)
+    # Resolve the hostname
+    try:
+        remote_ip = socket.gethostbyname(hostname)
 
-	except socket.error as error:
-		raise RuntimeError(error)
-	print('Resolved hostname ' + hostname + ' as being ' + remote_ip)
+    except socket.error as error:
+        raise RuntimeError(error)
+    print('Resolved hostname ' + hostname + ' as being ' + remote_ip)
 
-	# Connect to remote server
-	try:
-		sock.connect((remote_ip , port))
-	except socket.error as error:
-		raise RuntimeError(error)
+    # Connect to remote server
+    try:
+        sock.connect((remote_ip, port))
+    except socket.error as error:
+        raise RuntimeError(error)
 
-	print('Socket Connected to ' + hostname + ' on ip ' + remote_ip)
+    print('Socket Connected to ' + hostname + ' on ip ' + remote_ip)
+
 
 if __name__ == '__main__':
-	try:
-		s = create()
-	except RuntimeError as error:
-		print(error, sys.stderr)
-		sys.exit(1)
+    if len(sys.argv) < 3:
+        sys.exit(1)
+    host = sys.argv[1]
 
-	try:
-		connect(s, "www.google.fr", 80)
-		print('Socket Created')
-	except RuntimeError as error:
-		print(error, sys.stderr)
-		sys.exit(1)
-	finally:
-		s.close()
-		print('Socket Closed')
+    try:
+        port = int(sys.argv[2])
+    except Exception as error:
+        print(error)
+        sys.exit(1)
+
+    try:
+        s = create()
+    except RuntimeError as error:
+        print(error)
+        sys.exit(1)
+
+    try:
+        connect(s, host, port)
+        print('Socket Created')
+    except RuntimeError as error:
+        print(error)
+        sys.exit(1)
+    finally:
+        s.close()
+        print('Socket Closed')
--- pyclient/test_tcp.py
+++ pyclient/test_tcp.py
@@ -1,44 +1,46 @@
 import unittest
 import tcp
 
+
 class TestTcp(unittest.TestCase):
-	def test_create(self):
-		s = tcp.create()
-		self.assertNotEqual(s.fileno, -1)
-		s.close()
+    def test_create(self):
+        s = tcp.create()
+        self.assertNotEqual(s.fileno, -1)
+        s.close()
 
-	def test_connect(self):
-		s = tcp.create()
-		success = True
-		try:
-			tcp.connect(s, "google.fr", 80)
-		except:
-			success = False
-		finally:
-			s.close()
-		self.assertEqual(success, True)
+    def test_connect(self):
+        s = tcp.create()
+        success = True
+        try:
+            tcp.connect(s, "google.fr", 80)
+        except:
+            success = False
+        finally:
+            s.close()
+        self.assertEqual(success, True)
 
-	# def test_connect_fail_port(self):
-	# 	s = tcp.create()
-	# 	success = True
-	# 	try:
-	# 		tcp.connect(s, "google.fr", 8042)
-	# 	except:
-	# 		success = False
-	# 	finally:
-	# 		s.close()
-	# 	self.assertEqual(success, False)
+    # def test_connect_fail_port(self):
+    #     s = tcp.create()
+    #     success = True
+    #     try:
+    #         tcp.connect(s, "google.fr", 8042)
+    #     except:
+    #         success = False
+    #     finally:
+    #         s.close()
+    #     self.assertEqual(success, False)
 
-	def test_connect_fail_host(self):
-		s = tcp.create()
-		success = True
-		try:
-			tcp.connect(s, "gaagle.france", 80)
-		except:
-			success = False
-		finally:
-			s.close()
-		self.assertEqual(success, False)
+    def test_connect_fail_host(self):
+        s = tcp.create()
+        success = True
+        try:
+            tcp.connect(s, "gaagle.france", 80)
+        except:
+            success = False
+        finally:
+            s.close()
+        self.assertEqual(success, False)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
